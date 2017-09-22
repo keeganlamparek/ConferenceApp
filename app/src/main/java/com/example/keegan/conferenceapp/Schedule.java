30fa8e86ee8645521962c7463254bc130bb50341
DESKTOP-KF80L94\Keegan <keeganlamparek@gmail.com>
2017-09-21 21:22:36 -0500

    Initialized Schedule class in Android/Java Project.

---
+++ app/src/main/java/com/example/keegan/conferenceapp/Schedule.java
@@ -0,0 +1,10 @@
+package com.example.keegan.conferenceapp;
+
+/**
+ * Created by Keegan on 9/21/2017.
+ */
+
+public class Schedule {
+
+
+}
---
+++ app/src/test/java/com/example/keegan/conferenceapp/ScheduleTest.java
@@ -0,0 +1,10 @@
+package com.example.keegan.conferenceapp;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Keegan on 9/21/2017.
+ */
+public class ScheduleTest {
+
+}
