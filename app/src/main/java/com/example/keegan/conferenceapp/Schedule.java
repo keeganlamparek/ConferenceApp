c9f1ad7e2ca57608b96e62c8a774170b5e9f7851
KEEGANLAPTOP\Keegan <keegan.lamparek@my.simpson.edu>
2017-09-28 12:52:28 -0500

    Slightly updated object parameters for Schedule

--- app/src/main/java/com/example/keegan/conferenceapp/Schedule.java
+++ app/src/main/java/com/example/keegan/conferenceapp/Schedule.java
@@ -6,5 +6,22 @@ package com.example.keegan.conferenceapp;
 
 public class Schedule {
 
+    private String day;
+    private String timeBlock;
+    private String eventName;
 
+
+    public Schedule(String day ,String timeBlock, String eventName){
+        this.day = day;
+        this.timeBlock = timeBlock;
+        this.eventName = eventName;
+    }
+
+
+
+
+    @Override
+    public String toString() {
+        return "This time block is scheduled from ";
+    }
 }
