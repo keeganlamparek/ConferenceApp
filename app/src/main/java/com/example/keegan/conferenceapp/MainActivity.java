561ef3a285ea49e4e3426411bc800c880b8a71af
DESKTOP-KF80L94\Keegan <keeganlamparek@gmail.com>
2017-09-30 00:43:40 -0500

    Updated Schedule class.

--- app/src/main/java/com/example/keegan/conferenceapp/MainActivity.java
+++ app/src/main/java/com/example/keegan/conferenceapp/MainActivity.java
@@ -2,8 +2,18 @@ package com.example.keegan.conferenceapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.List;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemSelectedListener;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,6 +21,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Schedule example = new Schedule("9/29/17", "1:00 PM", "1:50 PM", "event1");
-        example.toString();
+
+        Spinner datesGroup = (Spinner) findViewById(R.id.dateSpinner);
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item, example.listOfDays);
+        dataAdapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        datesGroup.setAdapter(dataAdapter);
+
+
     }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        // On selecting a spinner item
+        String item = parent.getItemAtPosition(position).toString();
+        Toast.makeText(this, item, Toast.LENGTH_SHORT).show();
+        // Showing selected spinner item
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
+    }
+
+
 }
--- app/src/main/java/com/example/keegan/conferenceapp/Schedule.java
+++ app/src/main/java/com/example/keegan/conferenceapp/Schedule.java
@@ -9,17 +9,19 @@ import java.util.List;
 
 public class Schedule {
 
-    private String day;
+    public List<String> listOfDays = new ArrayList<>();
     private String timeBlock;
     private String eventName;
     private List<String> listOfTimeBlocks  = new ArrayList<>();
 
     public Schedule(String day, String timeBlockStart, String timeBlockEnd, String eventName){
-        this.day = day;
+        this.listOfDays.add(day);
         this.listOfTimeBlocks.add(timeBlockStart);
         this.listOfTimeBlocks.add(timeBlockEnd);
         this.eventName = eventName;
     }
+
+
 
     public void addTimeBlock(String timeBlock){
 
