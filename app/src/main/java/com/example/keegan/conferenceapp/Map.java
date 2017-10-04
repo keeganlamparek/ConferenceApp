d28eb90bcecd40d9bed04f9ca2e7bfa8134d5d37
Braeden Ingersoll <braeden.ingersoll@gmail.com>
2017-10-04 14:56:49 -0500

    Update to Map Class -9-28-17

--- app/src/main/java/com/example/keegan/conferenceapp/Map.java
+++ app/src/main/java/com/example/keegan/conferenceapp/Map.java
@@ -1,12 +1,28 @@
 package com.example.keegan.conferenceapp;
-
 /**
  * Created by Braeden Ingersoll on 9/21/2017.
  */
 
-public class Map {
+private class Map
+{
 
-    private String eventRoom;
-    private int roomNumber;
+    private String roomName;
     private String buildingName;
 }
+
+public Map (String eventRoom, int roomNumber, String buildingName)
+{
+    this.roomName = roomName;
+    this.buildingName = buildingName;
+}
+
+public String findMap(String buildingName, String roomName)
+{
+    if (buildingName.equals("Kent"))
+    {
+        if (roomName.equals("Carse") || roomName.equals("Black Box"))
+    }
+}
+}
+
+
