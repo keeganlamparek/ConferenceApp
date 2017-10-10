ba580cf224d6e9f64515d0518ce25f839026a626
Braeden Ingersoll <braeden.ingersoll@gmail.com>
2017-10-10 12:19:08 -0500

    Fixed Classes and Activities to be safe to merge.

---
+++ app/src/main/java/com/example/keegan/conferenceapp/KentFirstFloorActivity.java
@@ -0,0 +1,13 @@
+package com.example.keegan.conferenceapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KentFirstFloorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kent_first_floor);
+    }
+}
--- app/src/main/java/com/example/keegan/conferenceapp/Map.java
+++ app/src/main/java/com/example/keegan/conferenceapp/Map.java
@@ -3,26 +3,30 @@ package com.example.keegan.conferenceapp;
  * Created by Braeden Ingersoll on 9/21/2017.
  */
 
-private class Map
-{
+public class Map {
 
     private String roomName;
     private String buildingName;
-}
 
-public Map (String eventRoom, int roomNumber, String buildingName)
-{
-    this.roomName = roomName;
-    this.buildingName = buildingName;
-}
 
-public String findMap(String buildingName, String roomName)
-{
-    if (buildingName.equals("Kent"))
+    public Map(String eventRoom, int roomNumber, String buildingName) {
+        this.roomName = roomName;
+        this.buildingName = buildingName;
+    }
+
+    public String findMap(String buildingName, String roomName) {
+    /*if (buildingName.equals("Kent"))
     {
-        if (roomName.equals("Carse") || roomName.equals("Black Box"))
+        if (roomName.equals("Carse") || roomName.equals("Principal Black Box")) ||
+        {
+            String floorType = "Kent First Floor";
+        }
+        if (roomName.equals("Hubbel")) || roomName.equals("Student Senate")
+        {
+            String floorType = "Kent Second Floor";
+        }
+    }
+}*/
+        return ("Hi");
     }
 }
-}
-
-
