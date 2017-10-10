c8e0b9e1a348c73919d561f6d6c306158be60464
iamaname2 <matt.sebek@my.simpson.edu>
2017-10-10 12:17:41 -0500

    Small edit.

--- app/src/main/java/com/example/keegan/conferenceapp/Event.java
+++ app/src/main/java/com/example/keegan/conferenceapp/Event.java
@@ -11,39 +11,23 @@ public class Event {
     private String sessionLongDescription;
     private String trackDescription;
     private String trackSecondDescription;
-    private String presenterOneName;
-    private String presenterTwoName;
-    private String presenterThreeName;
-    private String presenterFourName;
+    private String[] presenters;
     private String buildingName;
     private int roomNumber;
     private String YearID;
-    private String blockDesc;
-    private String blockStart;
-    private String blockEnd;
-    private String blockStartEng;
-    private String blockEndEng;
     private String StatusDescription;
     private String sessionStatusNotes;
 
-    public Event(int sessionID, String sessionDescription, String sessionLongDescription, String trackDescription, String trackSecondDescription, String presenterOneName, String presenterTwoName, String presenterThreeName, String presenterFourName, String buildingName, int roomNumber, String yearID, String blockDesc, String blockStart, String blockEnd, String blockStartEng, String blockEndEng, String statusDescription, String sessionStatusNotes) {
+    public Event(int sessionID, String sessionDescription, String sessionLongDescription, String trackDescription, String trackSecondDescription, String[] presenters, String buildingName, int roomNumber, String yearID, String statusDescription, String sessionStatusNotes) {
         this.sessionID = sessionID;
         this.sessionDescription = sessionDescription;
         this.sessionLongDescription = sessionLongDescription;
         this.trackDescription = trackDescription;
         this.trackSecondDescription = trackSecondDescription;
-        this.presenterOneName = presenterOneName;
-        this.presenterTwoName = presenterTwoName;
-        this.presenterThreeName = presenterThreeName;
-        this.presenterFourName = presenterFourName;
+        this.presenters = presenters;
         this.buildingName = buildingName;
         this.roomNumber = roomNumber;
         this.YearID = yearID;
-        this.blockDesc = blockDesc;
-        this.blockStart = blockStart;
-        this.blockEnd = blockEnd;
-        this.blockStartEng = blockStartEng;
-        this.blockEndEng = blockEndEng;
         this.StatusDescription = statusDescription;
         this.sessionStatusNotes = sessionStatusNotes;
     }
