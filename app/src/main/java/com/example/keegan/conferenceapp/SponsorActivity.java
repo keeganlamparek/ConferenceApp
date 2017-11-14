0d7c5d15a98da3da828e2066a9d3a5e49134a608
iamaname2 <matt.sebek@my.simpson.edu>
2017-11-14 12:48:10 -0600

    Added Sponsor activity and images.  Images need to be renamed to have all lower case file path.

---
+++ app/src/main/java/com/example/keegan/conferenceapp/SponsorActivity.java
@@ -0,0 +1,31 @@
+package com.example.keegan.conferenceapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by matt.sebek on 11/14/2017.
+ */
+
+/*
+    This class will display sponsors of the Meca conference
+    This will be selectable from the navigation drawer
+    --Will this class hold any information other than images?--
+    --Maybe links to their websites from this page--
+ */
+
+public class SponsorActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
+        setContentView(R.layout.sponsor_activity);
+
+    }
+
+
+
+
+
+
+}
