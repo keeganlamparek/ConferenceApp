c68b427ebdda4d134467cdabea5e21ee3dc2090e
matt-PC\matt <matt.sebek@my.simpson.edu>
2017-12-05 15:21:50 -0600

    Main Activity fix.

--- app/src/main/java/com/example/keegan/conferenceapp/MainActivity.java
+++ app/src/main/java/com/example/keegan/conferenceapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.keegan.conferenceapp;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,6 +12,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -18,8 +20,8 @@ import com.google.firebase.database.FirebaseDatabase;
 public class MainActivity extends AppCompatActivity{
 
     private List<String> listOfDays;
-    GregorianCalendar[][] eventsOnDay1;
-    GregorianCalendar[][] eventsOnDay2;
+    static GregorianCalendar[][] eventsOnDay1 = new GregorianCalendar[1][5];
+    static GregorianCalendar[][] eventsOnDay2 = new GregorianCalendar[1][5];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,31 +60,30 @@ public class MainActivity extends AppCompatActivity{
             i.putExtra("day",passDay);
 
             startActivity(i);
-
-
-
         }
     };
 
     private void  buildSchedules(){
 
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
+
         // Build Days
         GregorianCalendar day1 = new GregorianCalendar(2017, 10, 12);
         GregorianCalendar day2 = new GregorianCalendar(2017, 10, 13);
 
         // Build start/end time arguments for TimeBlock class
         // Start times are first row.  End times are in second row.
-        // Columns seperates events.
-        eventsOnDay1[0][0] = new GregorianCalendar(2017, 10, 12, 9, 0);
-        eventsOnDay1[1][0] = new GregorianCalendar(2017, 10, 12, 10, 0);
+        // Columns separate events.
+        GregorianCalendar startTime1 = new GregorianCalendar(2017, 10, 12, 9, 0);
+
+        eventsOnDay1[0][0] = startTime1;
+/*        eventsOnDay1[1][0] = new GregorianCalendar(2017, 10, 12, 10, 0);
 
         eventsOnDay2[0][0] = new GregorianCalendar(2017, 10, 13, 9, 0);
         eventsOnDay2[1][0] = new GregorianCalendar(2017, 10, 13, 10, 0);
 
         eventsOnDay2[0][1] = new GregorianCalendar(2017, 10, 13, 10, 0);
-        eventsOnDay2[1][1] = new GregorianCalendar(2017, 10, 13, 11, 0);
+        eventsOnDay2[1][1] = new GregorianCalendar(2017, 10, 13, 11, 0);*/
 
         Schedule schedule1 = new Schedule(day1);
         Schedule schedule2 = new Schedule(day2);
@@ -92,21 +93,21 @@ public class MainActivity extends AppCompatActivity{
         listOfDays.add(schedule2.toString());
     }
 
-    public GregorianCalendar[][] getEventsOnDay1() {
+    public static GregorianCalendar[][] getEventsOnDay1() {
         return eventsOnDay1;
     }
 
-    public GregorianCalendar[][] getEventsOnDay2() {
+    public static GregorianCalendar[][] getEventsOnDay2() {
         return eventsOnDay2;
     }
 
 
     private void startWelcomeScreen() {
 
-        Intent startWelcomeScreen = new Intent(this, WelcomeScreen.class);
         WelcomeScreen welcome = new WelcomeScreen();
 
         if (!welcome.hasRun()){
+            Intent startWelcomeScreen = new Intent(this, WelcomeScreen.class);
             startActivity(startWelcomeScreen);
 
         }
